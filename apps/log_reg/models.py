163725e636d565bd33117004499331e148eedb21
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-10-18 15:53:02 -0700

    initial commit

---
+++ apps/log_reg/models.py
@@ -0,0 +1,56 @@
+from __future__ import unicode_literals
+from django.db import models
+import re
+
+class UsersManager(models.Manager):
+    def register_validator(self, postData):
+        errors = {}
+        NAME_REGEX = re.compile(r'^[a-zA-Z][a-zA-Z]+$')
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postData["first_name"]) < 1:
+            errors["first_name"] = "First name is required"
+        elif not NAME_REGEX.match(postData["first_name"]):
+            errors["first_name"] = "First name must be at least 2 letters and have no numbers"
+        if len(postData["last_name"]) < 1:
+            errors["last_name"] = "Last name is required"
+        elif not NAME_REGEX.match(postData["last_name"]):
+            errors["last_name"] = "Last name must be at least 2 letters and have no numbers"
+        if len(postData["email"]) < 1:
+            errors["email"] = "Email is required"
+        elif not EMAIL_REGEX.match(postData["email"]):
+            errors["email"] = "Invalid email"
+        elif Users.objects.filter(email = postData["email"]).count() > 0:
+            errors["email"] = "Email already exists"
+        if len(postData["password"]) < 1:
+            errors["pw"] = "Password is required"
+        elif len(postData["password"]) < 8:
+            errors["pw"] = "Password must be at least 8 characters"
+        if len(postData["confirm_password"]) < 1:
+            errors["confirm_pw"] = "You must confirm your password"
+        elif postData["password"] != postData["confirm_password"]:
+            errors["confirm_pw"] = "Password does not match"
+        return errors
+
+
+    def pw_validator(self, postData):
+        errors = {}
+        if len(postData["password"]) < 1:
+            errors["pw"] = "Password is required"
+        elif len(postData["password"]) < 9:
+            errors["pw"] = "Password must be at least 8 characters"
+        if len(postData["confirm_password"]) < 1:
+            errors["confirm_pw"] = "You must confirm your password"
+        elif postData["password"] != postData["confirm_password"]:
+            errors["confirm_pw"] = "Password does not match"
+        return errors
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    user_level = models.IntegerField()
+    password_hash = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    objects = UsersManager()
---
+++ apps/log_reg/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
+
+
+def index(request):
+    # request.session.flush()
+    return render(request, "log_reg/index.html")
+
+
+def signin(request):
+    return render(request, "log_reg/login.html")
+
+
+def login(request):
+    request.session["email"] = request.POST["email"]
+    if Users.objects.filter(email = request.POST["email"]).count() > 0:
+        user = Users.objects.get(email = request.POST["email"])
+        if bcrypt.checkpw(request.POST["password"].encode(), user.password_hash.encode()):
+            request.session["userid"] = user.id
+            request.session["first_name"] = user.first_name
+            request.session["last_name"] = user.last_name
+            return redirect("/dashboard")
+    messages.error(request, "You could not be logged in")
+    return redirect("/signin")
+
+
+def register(request):
+    return render(request, "log_reg/register.html")
+
+
+def registration(request):
+    if request.method == "POST":
+        request.session["first_name"] = request.POST["first_name"]
+        request.session["last_name"] = request.POST["last_name"]
+        request.session["email"] = request.POST["email"]
+        request.session["user_level"] = 1 
+        if Users.objects.all().count() == 0:
+            request.session["user_level"] = 9 
+        errors = Users.objects.register_validator(request.POST)
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect("/register")
+        pw_hash = bcrypt.hashpw(request.POST["password"].encode(), bcrypt.gensalt())
+        Users.objects.create(
+            first_name = request.POST["first_name"], 
+            last_name = request.POST["last_name"],
+            email = request.POST["email"],
+            user_level = request.session["user_level"],
+            password_hash = pw_hash
+        )
+    return redirect("/dashboard")
