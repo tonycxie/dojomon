b13327cd0e48b7568943ff4fb8ef5a1072319146
quezan <quezanw@gmail.com>
2018-10-24 12:50:43 -0700

    styled dashboard added todays time/date

--- apps/dashboard/static/dashboard/js/index.js
+++ apps/dashboard/static/dashboard/js/index.js
@@ -1,15 +1,33 @@
+function startTime() {
+    let date = new Date();
+    let hour = date.getHours();
+    if(hour > 12) {
+        hour -= 12;
+    }
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds)
+    let t = hour + ":" + minutes + ":" + seconds;
 
+    let today = date.toDateString();
+    $("#time").text(t);
+    $("#date").text(today);
 
+    let time = setTimeout(startTime, 500);
 
-
-
-
-
-
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i}
+        ;  // add zero in front of numbers < 10
+        return i;
+    }
+}
 
 $(document).ready(function () {
     $("#collapse-btn").click(function () { 
         $('#sidebar').toggleClass('active');
-        
     });
+    console.log("logged")
+    startTime();
 });
