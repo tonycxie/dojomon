e32f55b8bcbe116f9ee901b0d4f618cb970132fa
quezan <quezanw@gmail.com>
2018-10-24 16:55:32 -0700

    created simple edit team page

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^encounter$', views.encounter),
     url(r'^profile/(?P<id>\d+)$', views.profile_view),
     url(r'^save_sprite$', views.save_sprite),
-    url(r'^view_pokemon/(?P<id>\d+)$', views.view_pokemon)
+    url(r'^view_pokemon/(?P<id>\d+)$', views.view_pokemon),
+    url(r'^edit_team$', views.edit_page)
 ]
