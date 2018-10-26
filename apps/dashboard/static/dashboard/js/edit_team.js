f85bc746a6de8c3cc0f3b42a637bd2b84908d888
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-10-25 18:41:45 -0700

    made some small css fixes

--- apps/dashboard/static/dashboard/js/edit_team.js
+++ apps/dashboard/static/dashboard/js/edit_team.js
@@ -9,14 +9,25 @@ function addToTeam() {
                 console.log(response);
                 if (response) {
                     $("#player-party").append(
+                        // <div class="pokemon-wrapper row">
+                        //     <div class="pokemon-img col-3">
+                        //         <img src="{{sprite.front_sprite}}">
+                        //     </div>
+                        //     <p class="name col-7">{{sprite.name}}</p>
+                        //     <form action="/dashboard/remove_team" method="post" class="col-2 remove-pokemon">
+                        //         <input type="hidden" name="remove-id" class="remove-id" value="{{ sprite.id }}">
+                        //         <button class="btn" type="submit"><i class="fa fa-minus" aria-hidden="true"></i>
+                        //         </button>
+                        //     </form>
+                        // </div>
                         "<div class='pokemon-wrapper row'>" + 
                             "<div class='pokemon-img col-3'>" + 
                                 "<img src='" + response[0]["fields"]["front_sprite"] + "'>" + 
                             "</div>" + 
-                            "<p class='name col-4'>" + response[0]["fields"]["name"] + "</p>" + 
-                            "<form action='/dashboard/remove_team' method='post' class='col-1 remove-pokemon'>" +
+                            "<p class='name col-7'>" + response[0]["fields"]["name"] + "</p>" + 
+                            "<form action='/dashboard/remove_team' method='post' class='col-2 remove-pokemon'>" +
                                 "<input type='hidden' name='remove-id' id='remove-id' value='" + response[0]["pk"] + "'>" +
-                                "<button class='btn' type='submit'>-</button>" + 
+                                "<button class='btn' type='submit'><i class='fa fa-minus' area-hidden='true'></i></button>" + 
                             "</form>" +     
                         "</div>"
                     );
